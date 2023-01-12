207491cccb6fe0cc0141dde44976404dffc55b86
rupeshkumar <rupeshkumar9312@gmail.com>
2023-01-12 15:11:51 +0530

    question-04

---
+++ src/main/java/question04/Main.java
@@ -0,0 +1,9 @@
+package question04;
+
+public class Main{
+    public static void main(String[] args) {
+        Candidate candidate = new Candidate("Rupesh",29,"Btech");
+        Candidate candidate1 = new Candidate("abc",20,"Btech");
+        Candidate candidate2 = new Candidate("abbc",31,"B.Com");
+    }
+}
