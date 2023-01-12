fe03c3205859ea0c7ffe4c47e810eb426222034a
rupeshkumar <rupeshkumar9312@gmail.com>
2023-01-12 16:36:34 +0530

    question-09

---
+++ src/main/java/question09/InvalidMovieException.java
@@ -0,0 +1,8 @@
+package question09;
+
+public class InvalidMovieException extends RuntimeException{
+
+    public InvalidMovieException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/question09/Tester.java
@@ -0,0 +1,18 @@
+package question09;
+
+import java.util.HashMap;
+
+public class Tester {
+
+    public static void main(String[] args) {
+
+        HashMap<Integer,Integer> movieData = new HashMap<>();
+        movieData.put(101,120);
+        movieData.put(102,170);
+        movieData.put(103,150);
+
+        BookMyMovie bookMyMovie = new BookMyMovie(102,8);
+        double totalAmount = Discount.calculateTicketAmount(bookMyMovie,movieData);
+        System.out.println("Total amount for booking : " + totalAmount);
+    }
+}
